01c369c36b0b455a034b4f8a3d0763dc5a609d0e
Rama Malhis <rama.malhis@hotmail.com>
2024-06-03 14:07:47 +0200

    1d

---
+++ Exercise1.R
@@ -0,0 +1,9 @@
+#Strengths 
+# Very hard to lose files with git
+# Great for collaboration
+#Fast
+
+#Weaknesses 
+# complicated to use for biggeners 
+#Struggles with large files
+# Not good for storing data!
